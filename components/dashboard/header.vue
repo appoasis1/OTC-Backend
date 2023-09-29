d374ff2f23572f02aee080d1b348f7745f74cdd2
Nyasha Karata <nyashakarata35@gmail.com>
2023-09-29 13:30:17 +0200

    save point

--- components/dashboard/header.vue
+++ components/dashboard/header.vue
@@ -1,96 +1,105 @@
 <template>
-    <header class="header">
-        <div class="container-fluid">
-          <div class="row">
-            <div class=" col-4">
-              <div class="header-left d-flex align-items-center">
-                <div class="menu-toggle-btn mr-20">
-                  <button
-                    id="menu-toggle"
-                    class="main-btn btn-hover btn-outline"
-                  >
-                    <i class="pi pi-chevron-left me-2"></i> Menu
-                  </button>
-                </div>
-              </div> 
-            </div>
-            <div class="col-4 splitbutton">
-              <!-- <Button type="button"  icon="pi pi-calendar" class="p-button-warning"/>
-              <Button type="button"  icon="pi pi-clock" class="p-button-warning"/> -->
-           </div>
-           
-            <div class="col-4 splitbutton">
-               
-                <!-- <SplitButton :label="" icon="pi pi-user" :model="user_info"></SplitButton> -->
-                
-            </div>
+  <header class="header">
+      <div class="container-fluid">
+        <div class="row">
+          <div class=" col-4">
+            <div class="header-left d-flex align-items-center">
+              <div class="menu-toggle-btn mr-20">
+                <button
+                  id="menu-toggle"
+                  class="main-btn btn-hover btn-outline"
+                >
+                  <i class="pi pi-chevron-left me-2"></i> Menu
+                </button>
+              </div>
+            </div> 
+          </div>
+          <div class="col-4 splitbutton">
+            <Button type="button" :label="full_date()" icon="pi pi-calendar" class="p-button-warning"/>
+            <Button type="button" :label="full_time()" icon="pi pi-clock" class="p-button-warning"/>
+         </div>
+         
+          <div class="col-4 splitbutton">
+             
+              <SplitButton :label="`${full_name()} (${account_type})`" icon="pi pi-user" :model="user_info"></SplitButton>
+              
           </div>
         </div>
-        <Dialog header="Password Change" v-model:visible="passwordChange" :breakpoints="{'960px': '75vw', '640px': '90vw'}" :style="{width: '50vw'}" :modal="true">
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <Button label="Old Password"/>
-                    <Password :feedback="false" />
-                </div>
-            </div>
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <Button label="New Password"/>
-                    <Password >
-                    <template #header>
-                        <h6>Pick a password</h6>
-                    </template>
-                    <template #footer="">
-                        <Divider />
-                        <p class="mt-2">Suggestions</p>
-                        <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
-                            <li>At least one lowercase</li>
-                            <li>At least one uppercase</li>
-                            <li>At least one numeric</li>
-                            <li>Minimum 8 characters</li>
-                        </ul>
-                    </template>
-                </Password>
-                </div>
-            </div>
-            <div class="col-12 md:col-4">
-                <div class="p-inputgroup">
-                    <Button label="Confirm Password"/>
-                    <Password :feedback="false" />
-                </div>
-            </div>
-            <template #footer>
-              <Button  class="notmatch" label="Passwords Do Not Match"  autofocus />
-              <Button icon="pi pi-check" autofocus />
-            </template>
-        </Dialog>
-    </header>
+      </div>
+      <Dialog header="Password Change" v-model:visible="passwordChange" :breakpoints="{'960px': '75vw', '640px': '90vw'}" :style="{width: '50vw'}" :modal="true">
+          <div class="col-12 md:col-4">
+              <div class="p-inputgroup">
+                  <Button label="Old Password"/>
+                  <Password v-model="old_password" :feedback="false" />
+              </div>
+          </div>
+          <div class="col-12 md:col-4">
+              <div class="p-inputgroup">
+                  <Button label="New Password"/>
+                  <Password v-model="password">
+                  <template #header>
+                      <h6>Pick a password</h6>
+                  </template>
+                  <template #footer="">
+                      <Divider />
+                      <p class="mt-2">Suggestions</p>
+                      <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
+                          <li>At least one lowercase</li>
+                          <li>At least one uppercase</li>
+                          <li>At least one numeric</li>
+                          <li>Minimum 8 characters</li>
+                      </ul>
+                  </template>
+              </Password>
+              </div>
+          </div>
+          <div class="col-12 md:col-4">
+              <div class="p-inputgroup">
+                  <Button label="Confirm Password"/>
+                  <Password v-model="confirm_password" :feedback="false" />
+              </div>
+          </div>
+          <template #footer>
+            <Button v-if="password !== confirm_password" class="notmatch" label="Passwords Do Not Match"  autofocus />
+            <Button v-else label="Change Password" icon="pi pi-check" autofocus />
+          </template>
+      </Dialog>
+  </header>
 </template>
 
 <script setup lang="ts">
 import { useToast } from 'primevue/usetoast';
 import { storeToRefs } from "pinia";
 import moment from 'moment';
-   
-
+ 
 const toast = useToast();
 const first_name = ref();
 const last_name = ref();
+// const role = storeToRefs(authStore).role;
+const date = ref()
+const time = ref()
+const full_date = () => date.value
+const full_time = () => time.value
+const password = ref()
+const full_name = () => "NYASHA" + "  " + "KARATA"
+const account_type = ref("CLERK")
 const passwordChange = ref(false);
+const confirm_password = ref()
+const old_password = ref()
 const loading = ref(true)
-
+const access_levels  = ref()
 
 
 
 onMounted(async ()=> {
 
-  setInterval(() => nowTime(), 100)
-  setInterval(() => todayDate(),100)
+setInterval(() => nowTime(), 100)
+setInterval(() => todayDate(),100)
 });
 
 
 const closePassword = () => {
-  passwordChange.value = false
+passwordChange.value = false
 }
 // const changePassword = async () => {
 
@@ -111,84 +120,85 @@ const closePassword = () => {
 
 
 const todayDate = () => {
-  const timeElapsed = Date.now();
-  const today = new Date(timeElapsed);
-  const display = today.toDateString();
-  return display
+const timeElapsed = Date.now();
+const today = new Date(timeElapsed);
+const display = today.toDateString();
+date.value = display
+return display
 }
 
 
 
 const nowTime = () => {
-  const currentTime = new Date();
-  const  hours = currentTime.getHours();
-  let  minutes:any = currentTime.getMinutes();
+const currentTime = new Date();
+const  hours = currentTime.getHours();
+let  minutes:any = currentTime.getMinutes();
 
-	if (minutes < 10) {
-	 minutes = "0" + minutes;
-  }
-  let time = hours + ":" + minutes 
-  // authStore.time = time
-  return time
+if (minutes < 10) {
+ minutes = "0" + minutes;
+}
+let times = hours + ":" + minutes 
+time.value = times
+return times
 }
 
 const user_info = ref([
-				{
-					label: 'Change Password',
-					icon: 'pi pi-lock',
-					command: () => {
+      {
+        label: 'Change Password',
+        icon: 'pi pi-lock',
+        command: () => {
 
-            passwordChange.value = true
-						// this.$toast.add({severity:'success', summary:'Updated', detail:'Data Updated', life: 3000});
-					}
-				},
-        {
-					label: 'Edit Details',
-					icon: 'pi pi-user',
-					command: () => {
-						// window.location.href = 'https://vuejs.org/'
-					}
-				},
-				{
-					label: 'Log Out',
-					icon: 'pi pi-power-off',
-					command: () => {
-            logout()
-						// this.$toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000});
-					}
-				}
-			]);
+          passwordChange.value = true
+          // this.$toast.add({severity:'success', summary:'Updated', detail:'Data Updated', life: 3000});
+        }
+      },
+      {
+        label: 'Edit Details',
+        icon: 'pi pi-user',
+        command: () => {
+          // window.location.href = 'https://vuejs.org/'
+        }
+      },
+      {
+        label: 'Log Out',
+        icon: 'pi pi-power-off',
+        command: () => {
+          // logout()
+          // this.$toast.add({ severity: 'warn', summary: 'Delete', detail: 'Data Deleted', life: 3000});
+        }
+      }
+    ]);
 
 
-const logout = async () => {
-  // // const result = await authStore.logout();
-  
-  // if(result.success){
-  //   window.location.href = "/login";
-  //   if(!process.server){
-  //     localStorage.userData = "{}";
-  //   }
-  // }
-}
+// const logout = async () => {
+// const result = await authStore.logout();
+
+// if(result.success){
+//   window.location.href = "/login";
+//   if(!process.server){
+//     localStorage.userData = "{}";
+//   }
+// }
+// }
 </script>
 
 <style scoped>
 .splitbutton {
-    display: flex;
-    justify-content: flex-end;
+  display: flex;
+  justify-content: flex-end;
 }
 button.p-button.p-component.p-button-warning {
-    background-color: black;
-    margin-left: 5px;
-    border-color: white;
+  background-color: black;
+  margin-left: 5px;
+  border-color: white;
 }
 button.p-button.p-component.notmatch {
-    background-color: red;
-    width: 100%;
-    border-color: red;
+  background-color: red;
+  width: 100%;
+  border-color: red;
 }
 body {
-  zoom: 70%;
+zoom: 70%;
 }
 
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,112 +1,192 @@
 <template>
-    <NuxtLayout name="dashboard">
-        <section class="section">
-          <div class="container-fluid pt-5">
-             
+  <NuxtLayout name="dashboard">
+      <section class="section">
+        <div class="container-fluid pt-5">
+          <div class="grid">
+          <div class="col-12 lg:col-4 p-3">
+            <div class="shadow-2 surface-card p-3" style="border-radius: 12px;">
+              <div class="flex align-items-center justify-content-between">
+                <div>
+                  <span class="text-3xl text-900 font-bold">3</span>
+                  <p class="mt-2 mb-0 text-600 text-xl">Sales Invoices</p>
+                </div>
+                <div>
+                  <img src="https://blocks.primevue.org/images/blocks/illustration/stats-illustration-4.svg">
+                </div>
+              </div>
+            </div>
           </div>
-        </section>
-    </NuxtLayout>
-  </template>
-  
-  <script setup lang="ts">
-   
-  </script>
-  
-  <style>
-    .card-style {
-    background: #fff;
-    box-sizing: border-box;
-    padding: 25px 30px;
-    height: 100%;
+          <div class="col-12 lg:col-4 p-3">
+            <div class="shadow-2 surface-card p-3" style="border-radius: 12px;">
+              <div class="flex align-items-center justify-content-between">
+                <div>
+                  <span class="text-3xl text-900 font-bold">2</span>
+                  <p class="mt-2 mb-0 text-600 text-xl">Sales Orders</p>
+                </div>
+                <div>
+                  <img src="https://blocks.primevue.org/images/blocks/illustration/stats-illustration-5.svg">
+                </div>
+              </div>
+            </div>
+          </div>
+       
+        
+          <div class="col-12 lg:col-4 p-3">
+            <div class="shadow-2 surface-card p-3" style="border-radius: 12px;">
+              <div class="flex align-items-center justify-content-between">
+                <div>
+                  <span class="text-3xl text-900 font-bold">3</span>
+                  <p class="mt-2 mb-0 text-600 text-xl">Qutoations</p>
+                </div>
+                <div>
+                  <img src="https://blocks.primevue.org/images/blocks/illustration/stats-illustration-6.svg">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 lg:col-12 p-3">
+            <div class="flex flex-column md:flex-row gap-3 md:align-items-center bg-primary border-round-xl p-3">
+          <div class="flex flex-column align-items-center md:w-8rem">
+            <button class="p-button p-button-icon-only p-button-rounded p-button-sm bg-primary" data-pd-ripple="true">
+             
+              <span role="presentation" aria-hidden="true" data-p-ink="true" data-p-ink-active="false" class="p-ink" data-pc-name="ripple" data-pc-section="root"></span>
+            </button>
+            
+            <button class="p-button p-button-icon-only p-button-rounded p-button-sm bg-primary" data-pd-ripple="true">
+           
+              <span role="presentation" aria-hidden="true" data-p-ink="true" data-p-ink-active="false" class="p-ink" data-pc-name="ripple" data-pc-section="root"></span>
+            </button>
+          </div>
+          <div class="flex-1 gap-3 flex flex-column sm:flex-row">
+            <div class="flex-1 flex justify-content-center align-items-center bg-primary-400 shadow-1 p-3 border-round-md">
+              <div class="text-center">
+                <i class="pi pi-eye text-4xl mb-2"></i>
+                <div class="text-sm font-semibold mb-2">Sales Invoices</div>
+                <span class="font-semibold">6</span>
+              </div>
+            </div>
+            <div class="flex-1 flex justify-content-center align-items-center bg-primary-400 shadow-1 p-3 border-round-md">
+              <div class="text-center">
+                <i class="pi pi-users text-4xl mb-2"></i>
+                <div class="text-sm font-semibold mb-2">Sales Orders</div>
+                <span class="font-semibold">3</span>
+              </div>
+            </div>
+            <div class="flex-1 flex justify-content-center align-items-center bg-primary-400 shadow-1 p-3 border-round-md">
+              <div class="text-center">
+                <i class="pi pi-heart text-4xl mb-2"></i>
+                <div class="text-sm font-semibold mb-2">Sales Orders</div>
+                <span class="font-semibold">4</span>
+              </div>
+            </div>
+          </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </section>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+ 
+</script>
+
+<style>
+  .card-style {
+  background: #fff;
+  box-sizing: border-box;
+  padding: 25px 30px;
+  height: 100%;
+  position: relative;
+  border: 1px solid #fff;
+  box-shadow: 0px 10px 20px rgb(200 208 216 / 0%);
+  border-radius: 10px;
+  }
+  .row.dashtop {
+  margin-bottom: 30px;
+  }
+  .c-dashboardInfo {
+  margin-bottom: 15px;
+  }
+  .c-dashboardInfo .wrap {
+    background: #ffffff;
+    box-shadow: 2px 10px 20px rgb(0 0 0 / 10%);
+    border-radius: 5px;
+    text-align: center;
     position: relative;
-    border: 1px solid #fff;
-    box-shadow: 0px 10px 20px rgb(200 208 216 / 0%);
-    border-radius: 10px;
-    }
-    .row.dashtop {
-    margin-bottom: 30px;
-    }
-    .c-dashboardInfo {
-    margin-bottom: 15px;
-    }
-    .c-dashboardInfo .wrap {
-      background: #ffffff;
-      box-shadow: 2px 10px 20px rgb(0 0 0 / 10%);
-      border-radius: 5px;
-      text-align: center;
-      position: relative;
-      border: solid 1px white;
-      border-color: red solid 2px;
-      overflow: hidden;
-      padding: 40px 25px 20px;
-      height: 100%;
-  }
-    .c-dashboardInfo__title,
-    .c-dashboardInfo__subInfo {
-    color: #6c6c6c;
-    font-size: 1.18em;
-    }
-    .c-dashboardInfo span {
-    display: block;
-    }
-    .c-dashboardInfo__count {
-    font-weight: 600;
-    font-size: 2.5em;
-    line-height: 64px;
-    color: #323c43;
-    }
-    .c-dashboardInfo .wrap:after {
-      display: block;
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 15px;
-      content: "";
-  }
-  .loader{
-    position: fixed;
-    left: 0px;
-    top: 0px;
-    width: 100%;
+    border: solid 1px white;
+    border-color: red solid 2px;
+    overflow: hidden;
+    padding: 40px 25px 20px;
     height: 100%;
-    z-index: 9999;
-   }
-  
-    .c-dashboardInfo:nth-child(1) .wrap:after {
-      background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
-    }
-    .c-dashboardInfo:nth-child(2) .wrap:after {
-      background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
-    }
-    .c-dashboardInfo:nth-child(3) .wrap:after {
-      background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
-    }
-    .c-dashboardInfo:nth-child(4) .wrap:after {
-      background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
-    }
-    .c-dashboardInfo:nth-child(5) .wrap:after {
-      background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
-    }
-    .c-dashboardInfo__title svg {
-    color: #d7d7d7;
-    margin-left: 5px;
-    }
-    .MuiSvgIcon-root-19 {
-    fill: currentColor;
-    width: 1em;
-    height: 1em;
-    display: inline-block;
-    font-size: 24px;
-    transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
-    user-select: none;
-    flex-shrink: 0;
-    }
-   .icon-card{
-    width: 110%;
-  } 
-  .dashboard{
-    margin-right: 20px;
+}
+  .c-dashboardInfo__title,
+  .c-dashboardInfo__subInfo {
+  color: #6c6c6c;
+  font-size: 1.18em;
   }
-  
-  </style>
+  .c-dashboardInfo span {
+  display: block;
+  }
+  .c-dashboardInfo__count {
+  font-weight: 600;
+  font-size: 2.5em;
+  line-height: 64px;
+  color: #323c43;
+  }
+  .c-dashboardInfo .wrap:after {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 15px;
+    content: "";
+}
+.loader{
+  position: fixed;
+  left: 0px;
+  top: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: 9999;
+ }
+
+  .c-dashboardInfo:nth-child(1) .wrap:after {
+    background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
+  }
+  .c-dashboardInfo:nth-child(2) .wrap:after {
+    background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
+  }
+  .c-dashboardInfo:nth-child(3) .wrap:after {
+    background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
+  }
+  .c-dashboardInfo:nth-child(4) .wrap:after {
+    background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
+  }
+  .c-dashboardInfo:nth-child(5) .wrap:after {
+    background: linear-gradient(82.59deg, #6477c6 0%, #4a6cf7 100%);
+  }
+  .c-dashboardInfo__title svg {
+  color: #d7d7d7;
+  margin-left: 5px;
+  }
+  .MuiSvgIcon-root-19 {
+  fill: currentColor;
+  width: 1em;
+  height: 1em;
+  display: inline-block;
+  font-size: 24px;
+  transition: fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+  user-select: none;
+  flex-shrink: 0;
+  }
+ .icon-card{
+  width: 110%;
+} 
+.dashboard{
+  margin-right: 20px;
+}
+
+</style>
